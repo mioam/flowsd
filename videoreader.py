5aba3ad41ea84f5402e316d2434c589a96479efc
miaom <1678068845@qq.com>
2022-11-25 20:56:01 +0800

    支持参数

--- videoreader.py
+++ videoreader.py
@@ -1,20 +1,29 @@
 import cv2
 import os
+import argparse
 
-path = '859931305-1-80.flv'
-save_dir = 'frames'
-os.makedirs(save_dir, exist_ok=True)
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default='859931305-1-80.flv')
+    parser.add_argument('--save_dir', default='frames')
 
-cap = cv2.VideoCapture(path)
+    args = parser.parse_args()
 
-start = 300
-i = -start
-while (cap.isOpened() or i >= 1200):
-    ret, frame = cap.read()
-    if ret:
-        if i >=0 :
-            cv2.imwrite(os.path.join(save_dir, f'{i:06}.png'), frame)
-        i += 1
-    else:
-        break
-cap.release()
+    path = args.path
+    save_dir = args.save_dir
+    os.makedirs(save_dir, exist_ok=True)
+
+    cap = cv2.VideoCapture(path)
+
+    start = 300
+    i = -start
+    while (cap.isOpened() or i >= 1200):
+        ret, frame = cap.read()
+        if ret:
+            if i >=0 :
+                cv2.imwrite(os.path.join(save_dir, f'{i:06}.png'), frame)
+            i += 1
+        else:
+            break
+    cap.release()
